9a288437d4891c1dca396413d255a69c9f00c13d
Binal Bhatt <bhattgopi298@gmail.com>
2018-05-01 13:17:48 +0530

    spring_boot

---
+++ Admin-client-working/Admin-client-working/src/main/java/com/example/howtodoinjava/Adminclientworking/AdminClientWorkingApplication.java
@@ -0,0 +1,12 @@
+package com.example.howtodoinjava.Adminclientworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientWorkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClientWorkingApplication.class, args);
+	}
+}
